417f78139f2813a63ad4dcec224169c17db3e268
Anderson0x07 <andersonadrianorro@ufps.edu.co>
2022-11-21 23:01:02 -0500

    Dashboard inicial de administrador, se listan tablas

--- src/main/java/com/bodyhealth/controller/RutinaController.java
+++ src/main/java/com/bodyhealth/controller/RutinaController.java
@@ -1,10 +1,15 @@
 package com.bodyhealth.controller;
 
+import com.bodyhealth.model.Ejercicio;
 import com.bodyhealth.model.Musculo;
 import com.bodyhealth.model.Rutina;
+import com.bodyhealth.model.RutinaEjercicio;
+import com.bodyhealth.service.EjercicioService;
 import com.bodyhealth.service.MusculoService;
+import com.bodyhealth.service.RutinaEjercicioService;
 import com.bodyhealth.service.RutinaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,17 +17,32 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController
-@RequestMapping("/rutina")
+@Controller
+@RequestMapping("/admin")
 public class RutinaController {
     @Autowired
     private RutinaService rutinaService;
-    @GetMapping("/lista")
-    public List<Rutina> listarRutinas(Model model){
+    @Autowired
+    private RutinaEjercicioService rutinaEjercicioService;
+    @Autowired
+    private EjercicioService ejercicioService;
+
+    @GetMapping("/dash-rutinas")
+    public String listarRutinas(Model model){
         List<Rutina> rutinas = rutinaService.listarRutina();
 
         model.addAttribute("rutinas",rutinas);
+        model.addAttribute("rutina_ejercicios",listarRutinaEjercicio());
+        model.addAttribute("ejercicios",listarEjercicios());
 
-        return rutinas;
+        return "/admin/dash-rutinas";
+    }
+
+    public List<RutinaEjercicio> listarRutinaEjercicio(){
+        return rutinaEjercicioService.listarRutinasEjercicios();
+    }
+
+    public List<Ejercicio> listarEjercicios(){
+        return ejercicioService.listarEjercicios();
     }
 }
